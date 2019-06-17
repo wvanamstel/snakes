0ba84062fbca572af349915bec519f11fd97d910
wouter van amstel <wvanamstel@users.noreply.github.com>
2019-06-16 18:19:58 -0700

    stats

--- src/train.py
+++ src/train.py
@@ -4,31 +4,21 @@ import torch
 
 from glob import glob
 from PIL import Image
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from util import find_image_stats, SnakeData
 
 DATA_PATH = os.path.join(os.environ["LOCAL_DATA_PATH"], "crowdai", "snakes")
+# WRITER = SummaryWriter()
+np.random.seed(0)
 
 with open(os.path.join(DATA_PATH, "class_idx_mapping.csv"), "r") as f_in:
     classes = [cl.split(',') for cl in f_in.readlines()[1:]]
-    label_to_id = {_cl[1].rstrip(): _cl[0] for _cl in classes}
-
+    label_to_classname = {_cl[1].rstrip(): _cl[0] for _cl in classes}
 train_samples = glob(os.path.join(DATA_PATH, "train") + "/*/*", recursive=True)
+np.random.shuffle(train_samples)
+stats = find_image_stats(train_samples[:100])
 
-class SnakeData(Dataset):
-    def __init__(self, file_names, transformation=None):
-        self.file_names = file_names
-        self.transformation = transformation
-        self.label_to_id = label_to_id
-
-    def __len__(self):
-        return len(self.file_names)
-
-    def __get_item__(self, idx):
-        image_file = self.file_names[idx]
-        image = Image.open(image_file).convert("RGB")
-        _id = image_file.spli("/")[-2].split("-")[1]
-        if self.transformation:
-            self.transformation(image)
-        return image, _id
 
 
