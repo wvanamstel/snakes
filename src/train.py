991d07d6706a83fde714585bb3f78b1c2cad89b8
wouter van amstel <wvanamstel@users.noreply.github.com>
2024-08-12 17:55:22 -0700

    create stratified splits and dataloaders

--- src/train.py
+++ src/train.py
@@ -1,37 +1,45 @@
-import numpy as np
+import json
 import os
+from glob import glob
+
+import numpy as np
 import torch
+from PIL import Image
+from sklearn.model_selection import StratifiedShuffleSplit
+from torch.nn import Linear, CrossEntropyLoss
+from torch.utils.data import DataLoader, Dataset, Subset
+from torchvision import datasets
+from torchvision import models
+from torchvision import transforms as T
 
 from util import collate_fn
 
-from glob import glob
-from PIL import Image
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms as T
-
 DATA_PATH = os.path.join(os.environ["DATA_PATH"], "aicrowd", "snakes")
 
-SEGMENTATION_MODEL_PATH = os.path.join(os.environ["DATA_PATH"], "aicrowd/snakes/segm_model", "snake_seg.pt")
+SEGMENTATION_MODEL_PATH = os.path.join(
+    os.environ["DATA_PATH"], "aicrowd/snakes/segm_model", "snake_seg.pt"
+)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # SEG_MODEL = load_segmentation_model(SEGMENTATION_MODEL_PATH)
 
 
 # class SnakeData(Dataset):
-    # def __init__(self, file_names, transformation=None):
-        # self.file_names = file_names
-        # self.transformation = transformation
-        # self.label_to_id = label_to_id
+# def __init__(self, file_names, transformation=None):
+# self.file_names = file_names
+# self.transformation = transformation
+# self.label_to_id = label_to_id
 
-    # def __len__(self):
-        # return len(self.file_names)
+# def __len__(self):
+# return len(self.file_names)
 
-    # def __get_item__(self, idx):
-        # image_file = self.file_names[idx]
-        # image = Image.open(image_file).convert("RGB")
-        # _id = image_file.split("/")[-2].split("-")[1]
-        # if self.transformation:
-            # self.transformation(image)
-        # return image, _id
+# def __get_item__(self, idx):
+# image_file = self.file_names[idx]
+# image = Image.open(image_file).convert("RGB")
+# _id = image_file.split("/")[-2].split("-")[1]
+# if self.transformation:
+# self.transformation(image)
+# return image, _id
+
 
 def clf_image_transform(train: bool):
     transforms = []
@@ -49,7 +57,9 @@ def load_segmentation_model(model_path: str):
 
 
 class SnakeClfData(Dataset):
-    def __init__(self, file_names, label_to_id, transformation=None, segmentation_model_path=None):
+    def __init__(
+        self, file_names, label_to_id, transformation=None, segmentation_model_path=None
+    ):
         self.file_names = file_names
         self.transformation = transformation
         self.label_to_id = label_to_id
@@ -70,25 +80,77 @@ class SnakeClfData(Dataset):
         image_id = self.label_to_id[image_id]
         if self.seg_model:
             segmentation = self.seg_model(image)
-            mask = segmentation[0]['boxes'].to(torch.int)[0]
+            mask = segmentation[0]["boxes"].to(torch.int)[0]
             mask_height = mask[3] - mask[1]
             mask_width = mask[2] - mask[0]
             mask_top = mask[1]
             mask_left = mask[0]
-            image = T.functional.crop(image, mask_top, mask_left, mask_height, mask_width)
+            image = T.functional.crop(
+                image, mask_top, mask_left, mask_height, mask_width
+            )
         if self.transformation is not None:
             self.transformation(image)
         return image, image_id
 
 
+# Load the mapping for label to index and human readable name
 with open(os.path.join(DATA_PATH, "class_idx_mapping.csv"), "r") as f_in:
-    classes = [cl.split(',') for cl in f_in.readlines()[1:]]
+    classes = [cl.split(",") for cl in f_in.readlines()[1:]]
     label_to_snake_name = {_cl[1].rstrip(): _cl[0] for _cl in classes}
     label_to_idx = {label: i for i, label in enumerate(label_to_snake_name)}
 
-train_samples = glob(os.path.join(DATA_PATH, "train") + "/*/*", recursive=True)
+# Set up dataloader for train/test sets
+all_samples = glob(os.path.join(DATA_PATH, "train") + "/*/*", recursive=True)
 
-dataset = SnakeClfData(train_samples, label_to_idx, clf_image_transform(train=True), segmentation_model_path=SEGMENTATION_MODEL_PATH)
-data_loader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
+# The jpegs are corrupt
+with open(os.path.join(DATA_PATH, 'corrupt_img_files.json'), 'r') as f_in:
+    corrupt_images = json.load(f_in)
 
+# Remove corrupt images from samples
+valid_samples = list(set(all_samples).difference(set(corrupt_images)))
+
+# Find the sample indices for a stratified train/test split
+X = []
+y = []
+for sample in valid_samples:
+    split_sample = sample.split('/train/')
+    y_sample, X_sample = split_sample[1].split('/')
+    X.append(X_sample)
+    y.append(y_sample)
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
+train_indices, test_indices = next(strat_split.split(X, y))
+
+
+# Create the train and test datasets and dataloaders
+dataset = SnakeClfData(
+    valid_samples,
+    label_to_idx,
+    clf_image_transform(train=True),
+    segmentation_model_path=SEGMENTATION_MODEL_PATH,
+)
+
+train_dataset = Subset(dataset, train_indices)
+test_dataset = Subset(dataset, test_indices)
+
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
+test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False, collate_fn=collate_fn)
+
+
+
+
+#############################
+
+clf_model = models.resnet101(pretrained=True)
+in_features = clf_model.fc.in_features
+clf_model.fc = Linear(in_features, len(classes))
+clf_model = clf_model.to(DEVICE)
+
+crit = CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(clf_model.parameters(), lr=0.001, momentum=0.9)
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+num_of_epochs = 5
+# for epoch in range(num_of_epochs):
 
