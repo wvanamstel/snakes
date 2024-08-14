41fff17a54291072330aa6c82a44965cea3c32bd
wouter van amstel <wvanamstel@users.noreply.github.com>
2024-08-13 18:29:18 -0700

    training works

--- src/train.py
+++ src/train.py
@@ -18,45 +18,22 @@ SEGMENTATION_MODEL_PATH = os.path.join(
     os.environ["DATA_PATH"], "aicrowd/snakes/segm_model", "snake_seg.pt"
 )
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# SEG_MODEL = load_segmentation_model(SEGMENTATION_MODEL_PATH)
-
-
-# class SnakeData(Dataset):
-# def __init__(self, file_names, transformation=None):
-# self.file_names = file_names
-# self.transformation = transformation
-# self.label_to_id = label_to_id
-
-# def __len__(self):
-# return len(self.file_names)
-
-# def __get_item__(self, idx):
-# image_file = self.file_names[idx]
-# image = Image.open(image_file).convert("RGB")
-# _id = image_file.split("/")[-2].split("-")[1]
-# if self.transformation:
-# self.transformation(image)
-# return image, _id
-
-
-def clf_image_transform(train: bool):
-    transforms = []
-    # transforms.append(T.ToTensor())
-    if train:
-        transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
 
 
 # Define transforms for training and validation
 image_xforms = {
     "train": T.Compose(
         [
+            T.Resize(256),
+            T.CenterCrop(224),
             T.RandomHorizontalFlip(),
             T.Normalize([128, 120, 105], [54, 51, 50]),
         ]
     ),
     "test": T.Compose(
         [
+            T.Resize(256),
+            T.CenterCrop(224),
             T.Normalize([128, 120, 105], [54, 51, 50]),
         ]
     ),
@@ -88,22 +65,23 @@ class SnakeClfData(Dataset):
     def __getitem__(self, idx):
         image_file = self.file_names[idx]
         image = Image.open(image_file).convert("RGB")
-        image = T.ToTensor()(image).unsqueeze(0)
+        image = T.ToTensor()(image)
         image = image.to(DEVICE)
         image_id = image_file.split("/")[-2].split("-")[1]
         image_id = self.label_to_id[image_id]
         if self.seg_model:
-            segmentation = self.seg_model(image)
-            mask = segmentation[0]["boxes"].to(torch.int)[0]
-            mask_height = mask[3] - mask[1]
-            mask_width = mask[2] - mask[0]
-            mask_top = mask[1]
-            mask_left = mask[0]
-            image = T.functional.crop(
-                image, mask_top, mask_left, mask_height, mask_width
-            )
+            segmentation = self.seg_model(image.unsqueeze(0))
+            if segmentation[0]["boxes"].numel() != 0:
+                mask = segmentation[0]["boxes"].to(torch.int)[0]
+                mask_height = mask[3] - mask[1]
+                mask_width = mask[2] - mask[0]
+                mask_top = mask[1]
+                mask_left = mask[0]
+                image = T.functional.crop(
+                    image, mask_top, mask_left, mask_height, mask_width
+                )
         if self.transformation is not None:
-            self.transformation(image)
+            image = self.transformation(image)
         return image, image_id
 
 
@@ -142,38 +120,27 @@ dataset = {
         valid_samples,
         label_to_idx,
         image_xforms[x],
-        # clf_image_transform(train=True),
         segmentation_model_path=SEGMENTATION_MODEL_PATH,
     )
     for x in ["train", "test"]
 }
 
 
-# train_dataset = Subset(dataset['train'], train_indices)
-# test_dataset = Subset(dataset['test'], test_indices)
-
-# train_loader = DataLoader(
-# train_dataset, batch_size=2, shuffle=True, collate_fn=collate_fn
-# )
-# test_loader = DataLoader(
-# test_dataset, batch_size=2, shuffle=False, collate_fn=collate_fn
-# )
-
 shuffle_data = {"train": True, "test": False}
-phase_indices = {"train": train_indices, "test": test_indices}
+phase_indices = {"train": train_indices[:5000], "test": test_indices[:500]}
+batch_size = 40
 
 data_loader = {
     x: DataLoader(
         Subset(dataset[x], phase_indices[x]),
+        batch_size=batch_size,
         shuffle=shuffle_data[x],
-        # collate_fn=collate_fn,
     )
     for x in ["train", "test"]
 }
 
 
-#############################
-
+# Load resnet model and tune for specific number of classes
 clf_model = models.resnet101(pretrained=True)
 in_features = clf_model.fc.in_features
 clf_model.fc = Linear(in_features, len(classes))
@@ -181,12 +148,13 @@ clf_model = clf_model.to(DEVICE)
 
 crit = CrossEntropyLoss()
 
-optimizer = torch.optim.SGD(clf_model.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.Adam(clf_model.parameters(), lr=0.001)
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-num_of_epochs = 1
+num_of_epochs = 5
 for epoch in range(num_of_epochs):
-    print(f"Epoch: {epoch}/{num_of_epochs - 1}")
+    print("-" * 20)
+    print(f"Epoch: {epoch + 1}/{num_of_epochs}")
 
     for phase in ["train", "test"]:
         if phase == "train":
@@ -194,21 +162,34 @@ for epoch in range(num_of_epochs):
         else:
             clf_model.eval()
 
-        loss = 0.0
+        total_loss = 0.0
         correct = 0
 
         for inputs, labels in data_loader[phase]:
-            inputs.to(DEVICE)
-            labels.to(DEVICE)
+            labels = labels.to(DEVICE)
 
             optimizer.zero_grad()
 
             # Only calculate gradients in the training phase
-            with torch.set_grad_enabled(phase == 'train'):
+            with torch.set_grad_enabled(phase == "train"):
                 outputs = clf_model(inputs)
                 _, preds = torch.max(outputs, 1)
                 loss = crit(outputs, labels)
 
-                if phase == 'train':
+                if phase == "train":
                     loss.backward()
                     optimizer.step()
+
+            total_loss += loss.item() * inputs.size(0)
+            correct += torch.sum(preds == labels.data)
+
+        if phase == "train":
+            lr_scheduler.step()
+
+        epoch_loss = total_loss / len(phase_indices[phase])
+        epoch_accuracy = correct.double() / len(phase_indices[phase])
+
+        print(f"Phase: {phase}")
+        print(
+            f"Epoch loss: {epoch_loss}, accuracy: {epoch_accuracy}, correct: {correct}"
+        )
